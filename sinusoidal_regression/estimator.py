1b505d0d0e575e3158b9850f539b3839651d7e76
lmc2179 <louiscialdella@gmail.com>
2016-11-21 23:36:51 -0800

    added prediction method

--- sinusoidal_regression/estimator.py
+++ sinusoidal_regression/estimator.py
@@ -1,5 +1,7 @@
-from scipy.optimize import leastsq
+from scipy.optimize import leastsq, least_squares
 import numpy as np
+import pylab as plt
+import seaborn as sns
 
 def fit_sinusoidal_model(t, y):
     mean_guess = np.mean(y)
@@ -9,8 +11,19 @@ def fit_sinusoidal_model(t, y):
     std_mle, phase_mle, mean_mle = leastsq(f, [std_guess, phase_guess, mean_guess])[0]
     return std_mle, phase_mle, mean_mle
 
+def predict(t, std_mle, phase_mle, mean_mle):
+    data_fit = std_mle * np.sin(t + phase_mle) + mean_mle
+    return data_fit
+
 if __name__ == '__main__':
     N = 1000  # number of data points
-    t = np.linspace(0, 4 * np.pi, N)
-    y = 3.0 * np.sin(t + 0.001) + 0.5 + np.random.randn(N)  # create artificial data with noise
-    print(fit_sinusoidal_model(t, y))
+    T = np.linspace(0, 4 * np.pi, N)
+    y = 3.0 * np.sin(T + 0.001) + 0.5 + np.random.randn(N)  # create artificial data with noise
+    params = fit_sinusoidal_model(T, y)
+    print(params)
+    y_predicted = predict(T, *params)
+    plt.plot(y, '.')
+    plt.plot(y_predicted, label='after fitting')
+    plt.legend()
+    plt.show()
+
