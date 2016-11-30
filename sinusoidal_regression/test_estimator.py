4a0bfd06dacd0ac88115ac5107803cc67fb4718c
lmc2179 <louiscialdella@gmail.com>
2016-11-30 08:09:13 -0800

    refactored test for readability

--- sinusoidal_regression/test_estimator.py
+++ sinusoidal_regression/test_estimator.py
@@ -6,11 +6,15 @@ class SingleWaveRegression(unittest.TestCase):
     def test_regression(self):
         N = 5000  # number of data points
         T = np.linspace(0, 4 * np.pi, N)
-        y = 3.0 * np.sin(1.2*T + 0.1) + 0.5 + np.random.randn(N)  # create artificial data with noise
+        TRUE_AMPLITUDE = 3.0
+        TRUE_FREQUENCY = 1.2
+        TRUE_PHASE = 0.1
+        TRUE_MEAN = 0.5
+        y = TRUE_AMPLITUDE * np.sin(TRUE_FREQUENCY*T + TRUE_PHASE) + TRUE_MEAN + np.random.randn(N)
         m = SinusoidalRegression()
         m.fit_sinusoidal_model(T, y)
         std_mle, frequency_mle, phase_mle, mean_mle = m.params_
-        self.assertAlmostEqual(std_mle, 3.0, places=1)
-        self.assertAlmostEqual(phase_mle, 0.1, places=1)
-        self.assertAlmostEqual(mean_mle, 0.5, places=1)
-        self.assertAlmostEqual(frequency_mle, 1.2, places=1)
+        self.assertAlmostEqual(std_mle, TRUE_AMPLITUDE, places=1)
+        self.assertAlmostEqual(phase_mle, TRUE_PHASE, places=1)
+        self.assertAlmostEqual(mean_mle, TRUE_MEAN, places=1)
+        self.assertAlmostEqual(frequency_mle, TRUE_FREQUENCY, places=1)
